b96b8c5e6274e2ccf5c230c3db678740a316396f
winuss <niceyss@gmail.com>
2020-04-28 14:00:57 +0900

    gatsby init

---
+++ config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    title: `DevTimes`,
+    siteUrl: `https://devtimes.com`,
+    description: `A Development for %TOPICS%`,
+    topics: [`angular`, `bigdata`, 'game'],
+    menu: [
+        {
+            name: 'All',
+            path: '/archive'
+          },
+          {
+            name: 'Web',
+            path: '/tag/web'
+          },
+          {
+            name: 'BigData',
+            path: '/tag/bigdata'
+          },
+          {
+            name: 'Demo',
+            path: '/tag/demo'
+          },
+          {
+            name: 'Example',
+            path: '/page'
+          },
+    ],
+    footerMenu: [
+        {
+            name: 'RSS',
+            path: '/rss.xml'
+        },
+        {
+            name: 'Sitemap',
+            path: '/sitemap.xml'
+        }
+    ],
+    search: true,
+    author: {
+        name: `winuss`,
+        description: `I'm <strong>yoo seongsu</strong>, every end is a new beginning.
+            <a href="https://devtimes.com" rel="noopener" target="_blank">devtimes.com</a>`,
+        social: {
+            facebook: ``,
+            twitter: `https://twitter.com/winuss`,
+            linkedin: ``,
+            instagram: ``,
+            youtube: ``,
+            github: `https://github.com/winuss`,
+            twitch: ``
+        }
+    }
+}
