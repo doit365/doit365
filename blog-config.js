8afa85ff4b48b043d513b338089ed6afd96644f5
winuss <nicyes@naver.com>
2021-12-24 22:04:08 +0900

    sns

--- blog-config.js
+++ blog-config.js
@@ -7,10 +7,10 @@ module.exports = {
     github: "https://github.com/niceyss",
     kaggle: "https://kaggle.com",
     linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com/",
-    instagram: "https://www.instagram.com/",
+    // facebook: "https://www.facebook.com/",
+    // instagram: "https://www.instagram.com/",
     email: "mailto:niceyss@gmail.com",
-    etc: "https://www.notion.so/",
+    etc: "https://www.notion.so/"
   },
   utterances: {
     repo: "winuss/winuss.github.io",
